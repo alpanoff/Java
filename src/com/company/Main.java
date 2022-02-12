084cc812cf9cbb378dd53e5ed05d7b63f771555a
alpanoff <alexandr.panov@gmail.com>
2022-02-12 23:03:48 +0500

    main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Person[] office = {
+                mainEmployee,
+                new Person("Андрей", "Андреевич",
+                        "Андреев", "8(495)111-22-33",
+                        "project owner", 520000, 1973),
+                new Person("Евгений", "Евгеньевич",
+                        "Евгеньев", "8(495)222-33-44",
+                        "project manager", 40000, 1963),
+                new Person("Наталья", "Натальевна",
+                        "Натальева", "8(495)333-44-55",
+                        "senior developer", 90000, 1990),
+                new Person("Тетенька", "Тетеньковна",
+                        "Тетенькова", "8(495)444-55-66",
+                        "engineer", 50000, 1983)
+        }
+        Course c = new Course(...); // Создаем полосу препятствий
+        Team team = new Team(...); // Создаем команду
+        c.doIt(team); // Просим команду пройти полосу
+        team.showResults(); // Показываем результаты
+    }
 
     }
 }
